df25f0a90adfcb5409641107b30dcfbe37b9ea49
Robert de Jesus Araújo <robertjesus2009@gmail.com>
2023-10-18 11:09:04 -0300

    Documentação

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"metodos"},{"p":"<Unnamed>","l":"OperadoresLogicos"},{"p":"<Unnamed>","l":"OperadoresRelacionais"},{"p":"<Unnamed>","l":"TiposOperadores"},{"p":"<Unnamed>","l":"TiposVariaveis"}];updateSearchResults();
